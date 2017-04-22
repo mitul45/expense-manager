0792d0428b017b3d14045fb465ff769d8df4792a
Mitul Shah <45.mitul@gmail.com>
2017-04-22 07:15:34 +0000

    rebuild pages at dd15c5c

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Mitul Shah"
+git config user.email "45.mitul@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/mitul45/expense-manager.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
