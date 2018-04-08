0f5a2f3858fc8ebdc10b91a44ba33c318b715e6d
Mitul Shah <mitul.shah@booking.com>
2018-04-08 13:09:18 +0200

    Add income in the bar chart

--- plot-charts.js
+++ plot-charts.js
@@ -13,26 +13,29 @@
     'Nov',
     'Dec'
   ]
-  let overallChart;
+  let monthlyBarChart;
 
   function getMonthlyExpense(expenses, category) {
     const today = new Date();
     const data = {
       labels: [],
       expense: [],
+      income: [],
     };
     const indexDate = new Date();
     indexDate.setMonth(today.getMonth() - 11);
 
     for (let i = 0; i < 12; i++) {
       let monthlyExpense = 0;
+      let montlyIncome = 0;
       const indexMonth = indexDate.getMonth();
       const indexYear = indexDate.getFullYear();
       expenses.forEach(expense => {
         const expenseDate = expense[0];
         const expenseCategory = expense[3];
         const isTransfer = expense[6];
-        const amount = expense[4];
+        const expenseAmount = expense[4];
+        const incomeAmount = expense[5];
         if (!isTransfer) {
           const expenseMonth = expenseDate.getMonth();
           const expenseYear = expenseDate.getFullYear();
@@ -40,18 +43,21 @@
           if (expenseMonth === indexMonth && expenseYear === indexYear) {
             if (category && category !== "All") {
               if (expenseCategory === category) {
-                monthlyExpense += amount;
+                monthlyExpense += expenseAmount;
+                montlyIncome += incomeAmount;
               }
             } else {
-              monthlyExpense += amount;
+              monthlyExpense += expenseAmount;
+              montlyIncome += incomeAmount;
             }
           }
         }
       })
 
-      if (monthlyExpense) {
+      if (monthlyExpense || montlyIncome) {
         data.labels.push(`${months[indexMonth]} ${indexYear}`);
         data.expense.push(monthlyExpense.toFixed(2));
+        data.income.push(montlyIncome.toFixed(2));
       }
 
       indexDate.setMonth(indexDate.getMonth() + 1);
@@ -63,21 +69,31 @@
   function updateData(chart, data) {
     chart.data.labels = data.labels;
     chart.data.datasets[0].data = data.expense;
+    chart.data.datasets[1].data = data.income;
     chart.update();
   }
 
   function ployMontlyExpenseChart(allExpenses, category) {
     const ctx = document.querySelector(".details__overall canvas").getContext('2d');
     const data = getMonthlyExpense(allExpenses, category);
-    if (overallChart) {
-      updateData(overallChart, data);
+    if (monthlyBarChart) {
+      updateData(monthlyBarChart, data);
     } else {
-      overallChart = new Chart(ctx, {
-        type: 'bar',
+      monthlyBarChart = new Chart(ctx, {
+        type: 'horizontalBar',
         data: {
           labels: data.labels,
-          datasets: [{
+          datasets: [
+          {
             data: data.expense,
+            label: "Expense",
+            backgroundColor: "#ED5E59",
+            borderWidth: 1
+          },
+          {
+            data: data.income,
+            label: "Income",
+            backgroundColor: "#67AB5B",
             borderWidth: 1
           }]
         },
